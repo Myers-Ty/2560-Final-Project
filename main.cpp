4cdd9ecb17b264b3ec93b9a5c713bd159098e1cf
HAlex753 <167333285+HAlex753@users.noreply.github.com>
2024-11-11 03:55:23 +0000

    Add files via upload
    Created method to parse CSV file of officer data. Created CSV file of officers.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-// Compile program with g++ Main.cpp -o EmergResponse -I/usr/local/opt/curl/include -L/usr/local/opt/curl/lib -lcurl  `pkg-config --cflags --libs opencv4`
 #include <iostream>
 #include <curl/curl.h> // Used for Map generation : Used Ubuntu to install cURL
 #include <fstream>
@@ -7,11 +6,14 @@
 #include <opencv2/core/core.hpp> // Install with Ubuntu
 #include <opencv2/highgui/highgui.hpp>  // Install with Ubuntu  
 #include <nlohmann/json.hpp> // Install with Ubuntu
+#include <vector>
+#include <fstream>
 
 class NortheasternEmergency
 {
     private:
         static std::string apiKey;
+        std::vector<std::string> NUPDLocations;
 
         // Used to encode the overivew polyline
         std::string urlEncode(const std::string &value) 
@@ -60,7 +62,7 @@ class NortheasternEmergency
                 curl_easy_cleanup(curlPoly);
                 // Parse the JSON response to extract the polyline
                 nlohmann::json jsonResponse = nlohmann::json::parse(readBufferPoly);
-                std::cout << readBufferPoly << std::endl;
+                // std::cout << readBufferPoly << std::endl; // Used for DEBUGGING
                 if (!jsonResponse["routes"].empty()) {
                     polyline = jsonResponse["routes"][0]["overview_polyline"]["points"].get<std::string>();
                     std::cout << "polyline found" << std::endl; // Used for DEBUGGING
@@ -70,10 +72,6 @@ class NortheasternEmergency
             }
             return urlEncode(polyline);
         }
-        
-
-
-        // Get distance of polylines
         double getPolyLineDistance(std::string origin, std::string destination)
         {
             // Construct the URL for the Directions API
@@ -179,6 +177,7 @@ class NortheasternEmergency
                 curl_easy_cleanup(curlURL);
                 // Parse the JSON response to extract the polyline
                 nlohmann::json jsonResponse = nlohmann::json::parse(readBufferURL);
+                std::cout << readBufferURL << std::endl; // Used for Debugging
                 if (!jsonResponse["predictions"].empty()) {
                     PlaceID = jsonResponse["predictions"][0]["description"].get<std::string>();
                 } else {
@@ -190,75 +189,148 @@ class NortheasternEmergency
             return PlaceID;
         }
 
-        static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp)
-        {
+        static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp) {
             ((std::string*)userp)->append((char*)contents, size * nmemb);
             return size * nmemb;
         }
         
-    public:
-        NortheasternEmergency()
+        void DynamicOfficerAllocation()
         {
 
         }
 
-        void ShortestPath(std::string origin, std::string destination)
+        void ParseOfficersCSV(std::string PathToOfficersCSV)
         {
-            // Format the origin and destination to match Google's API Format
-            std::replace(origin.begin(), origin.end(), ' ', '+'); // replace all ' ' to '+'
-            std::replace(destination.begin(), destination.end(), ' ', '+'); // replace all ' ' to '+'
+            std::ifstream file(PathToOfficersCSV);
+            std::string line;
+            if (file.is_open())
+            {
+                while (getline(file, line))
+                {
+                    std::stringstream ss(line);
+                    std::string ID, location, isAvailableStr;
+                    bool isAvailable;
 
-            origin = autocompleteAddress(origin);
-            destination = autocompleteAddress(destination);
-            std::cout << getPolyLine(origin, destination) << std::endl;
-            std::string url = "https://maps.googleapis.com/maps/api/staticmap?"
-                        "size=600x600"
-                        "&markers=color:blue%7Clabel:S%7C" + origin + // Add NEU to address for accuracy
-                        "&markers=color:red%7Clabel:D%7C" + destination + // Add NEU to address for accuracy
-                        "&path=enc:" + getPolyLine(origin, destination) + // Shows actual path for officers to take
-                        "&maptype=satellite" // Change map type to satellite
-                        //"&zoom=17" // Adjust zoom level to focus on the path
-                        "&key=" + this->apiKey;
-            std::cout << url << std::endl; //Used for DEBUGGING
-            CURL* curl;
-            CURLcode res;
-            std::string readBuffer;
-            curl = curl_easy_init();
-            if(curl) {
-                curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-                curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-                curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
-                res = curl_easy_perform(curl);
-                curl_easy_cleanup(curl);
+                    getline(ss, ID, ',');
+                    getline(ss, location, ',');
+                    getline(ss, isAvailableStr);
+
+                    if (isAvailableStr == "true")
+                    {
+                        isAvailable = true;
+                    }
+                    else
+                    {
+                        isAvailable = false;
+                    }
+
+                    Officer officer;
+                    officer.ID = ID;
+                    officer.location = location;
+                    officer.isAvailable = isAvailable;
+                    officers.push_back(officer);
+                }
+                file.close();
             }
 
-            // Save the image to a file
-            std::ofstream outFile("route_map.png", std::ios::binary);
-            outFile.write(readBuffer.c_str(), readBuffer.size());
-            outFile.close();
-            cv::Mat image= cv::imread("route_map.png");   
-            std::string windowName = "Emergency Route"; //Name of the window
-            cv::namedWindow(windowName); // Create a window
-            cv::imshow(windowName, image); // Show our image inside the created window.
-            cv::waitKey(0); // Wait for any keystroke in the window
-            cv::destroyWindow(windowName); //destroy the created window
+            else
+            {
+                std::cout << "File failed to open" << std::endl;
+            }
+        }
+        void ParseCSV(std::string PathToCSV)
+        {
+            std::ifstream file(PathToCSV); 
+            
+            if (file.is_open()) {
+                std::string Location;
+                std::string Emergency;
+                while (getline(file, Location, ',')) 
+                {
+                    std::cout << "Location:" << Location << std::endl ; 
+
+                    getline(file, Emergency);
+                    std::cout << "Emergency:" << Emergency << std::endl  ; 
+
+                    // getline(file, Equipment, ','); // can just add rest of getline commands right inside this loop
+                }
+            }
+            else {
+                std::cout << "File failed to open" << std::endl;
+            }
         }
 
+    public:
+    NortheasternEmergency(std::string PathToCSV, std::string PathToOfficersCSV)
+    {
+        ParseCSV(PathToCSV);
+        ParseOfficersCSV(PathToOfficersCSV);
+        DynamicOfficerAllocation();
+    }
+
+    void ShortestPath(std::string origin, std::string destination)
+    {
+        // Format the origin and destination to match Google's API Format
+        std::replace(origin.begin(), origin.end(), ' ', '+'); // replace all ' ' to '+'
+        std::replace(destination.begin(), destination.end(), ' ', '+'); // replace all ' ' to '+'
+
+        origin = autocompleteAddress(origin);
+        destination = autocompleteAddress(destination);
+        // std::cout << getPolyLine(origin, destination) << std::endl; // Used for Debugging
+        std::string url = "https://maps.googleapis.com/maps/api/staticmap?"
+                    "size=600x600"
+                    "&markers=color:blue%7Clabel:S%7C" + origin + // Add NEU to address for accuracy
+                    "&markers=color:red%7Clabel:D%7C" + destination + // Add NEU to address for accuracy
+                    "&path=enc:" + getPolyLine(origin, destination) + // Shows actual path for officers to take
+                    "&maptype=satellite" // Change map type to satellite
+                    //"&zoom=17" // Adjust zoom level to focus on the path
+                    "&key=" + this->apiKey;
+        // std::cout << url << std::endl; //Used for DEBUGGING
+        CURL* curl;
+        CURLcode res;
+        std::string readBuffer;
+        curl = curl_easy_init();
+        if(curl) {
+            curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+            res = curl_easy_perform(curl);
+            curl_easy_cleanup(curl);
+        }
+        // Save the image to a file
+        std::ofstream outFile("route_map.png", std::ios::binary);
+        outFile.write(readBuffer.c_str(), readBuffer.size());
+        outFile.close();
+        cv::Mat image= cv::imread("route_map.png");   
+        std::string windowName = "Emergency Route"; //Name of the window
+        cv::namedWindow(windowName); // Create a window
+        cv::imshow(windowName, image); // Show our image inside the created window.
+        cv::waitKey(0); // Wait for any keystroke in the window
+        cv::destroyWindow(windowName); //destroy the created window
+    }
+
     
+   
 };
 
 std::string NortheasternEmergency::apiKey = "AIzaSyAVzi2oft7sKVPwi75u-gat3_uk-cwsEB8"; // Default set to Tyler's API Key
 
 int main()
 {
-    NortheasternEmergency emerg;
+    // Replace paths with paths to your .csv files
+    NortheasternEmergency emerg("PastEmergencies.csv", "officers.csv");
+    /*
     while (true)
     {
         std::string location;
-        std::cout << "Enter the destination: ";
+        std::string location2;
+        std::cout << "Enter the origin: ";
         std::getline(std::cin, location);
-        emerg.ShortestPath("Ell Hall", location);
+        std::cout << "Enter the destination: ";
+        std::getline(std::cin, location2);
+        emerg.ShortestPath(location, location2);
     }
     emerg.ShortestPath("Ell Hall", "Snell Library");
+    */
     return 0;
 }
